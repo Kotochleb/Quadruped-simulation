4bbcfe36b547e0877f095a8968a73105944af787
kotochleb <krzy.wojciecho@gmail.com>
2020-04-29 13:30:19 +0200

    normal force added

--- objects/Isdone.m
+++ objects/Isdone.m
@@ -1,20 +1,20 @@
 classdef Isdone
    properties
-        wx_threshold        {mustBeNumeric}  
-        wx_gain             {mustBeNumeric}  
-        wy_threshold        {mustBeNumeric}  
-        wy_gain             {mustBeNumeric}  
-        wz_threshold        {mustBeNumeric}  
-        wz_gain             {mustBeNumeric}
+        wx_threshold                {mustBeNumeric}  
+        wx_gain                     {mustBeNumeric}  
+        wy_threshold                {mustBeNumeric}  
+        wy_gain                     {mustBeNumeric}  
+        wz_threshold                {mustBeNumeric}  
+        wz_gain                     {mustBeNumeric}
         
-        fall_height         {mustBeNumeric}  
-        fall_gain           {mustBeNumeric}
+        fall_height                 {mustBeNumeric}  
+        fall_gain                   {mustBeNumeric}
         
-        hip_max_angle       {mustBeNumeric}  
-        hip_fall_height     {mustBeNumeric}  
-        hip_angle_gain      {mustBeNumeric}
+        hip_contact_force_threshold {mustBeNumeric}
+        hip_contact_gain            {mustBeNumeric}
+
         
-        err_int_threshold   {mustBeNumeric}  
+        err_int_threshold           {mustBeNumeric}  
     
    end
 end
